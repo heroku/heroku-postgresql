c9e3e86b4c2cd3c7db21adf88270533301f079db
Noah Zoschke <noah@heroku.com>
2010-10-21 17:13:28 -0700

    fix requires

--- init.rb
+++ init.rb
@@ -1,6 +1,27 @@
-require "lib/heroku-postgresql/client"
-require "lib/pgbackups/client"
-require "lib/heroku/commands/base"      # monkey-patch confirm-command until appears upstream
-require "lib/heroku/commands/pgutils"
-require "lib/heroku/commands/pgbackups"
-require "lib/heroku/commands/pg"
+require "heroku-postgresql/client"
+require "pgbackups/client"
+require "heroku/commands/pgutils"
+require "heroku/commands/pgbackups"
+require "heroku/commands/pg"
+
+# monkey-patch in confirm_command until accepted upstread
+module Heroku::Command
+  class Base
+    def confirm_command(message="This command requires confirmation.")
+      if extract_option('--force')
+        display "Command execution forced. Proceeding."
+        return true
+      end
+
+      confirmed_app = extract_option('--confirm', false)
+      display message
+      unless confirmed_app == app
+        raise(CommandFailed, "Add '--confirm #{app}' to execute this command.")
+      else
+        display("Command confirmed for #{app}. Proceeding.")
+      end
+
+      true
+    end
+  end
+end
